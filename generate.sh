32cb85b6dfb190a98923fb23b4ecb853787c2f86
jld3103 <jld3103yt@gmail.com>
2019-07-02 17:46:59 +0200

    Fix asset generation script

--- generate.sh
+++ generate.sh
@@ -7,6 +7,7 @@ sed -i -- 's/ffffff/5bc638/g' images/logo_green.svg
 convert -resize 1024x1024 -background none images/logo_white.svg images/logo_white.png
 convert -resize 1024x1024 -background none images/logo_green.svg images/logo_green.png
 # Create app icons
+flutter pub get
 flutter pub run flutter_launcher_icons:main
 cp images/logo_white.png android/app/src/main/res/drawable/
 cd ..
