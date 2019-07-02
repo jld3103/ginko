8af070bab0e2908db1b19a56d8dde04697f2a37b
jld3103 <jld3103yt@gmail.com>
2019-07-02 17:19:23 +0200

    Add test coverage

---
+++ test-coverage.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mkdir -p coverage
+rm -rf coverage/*
+
+folders=("models" "server" "app" )
+for d in ${folders[@]} ; do
+    cd $d
+
+    flutter test --coverage ../tests || error=true
+
+    escapedPath="$(echo $d | sed 's/\//\\\//g')"
+    sed "s/^SF:lib/SF:$escapedPath\/lib/g" coverage/lcov.info >> ../coverage/lcov.info
+    rm coverage -r
+
+    # Fail the build if there was an error
+    if [[ "$error" = true ]]; then
+      exit -1
+    fi
+    cd ..
+done
+
+if ! [ "$TRAVIS" ]; then
+  genhtml coverage/lcov.info -o coverage --no-function-coverage -s -q -p `pwd`
+fi
